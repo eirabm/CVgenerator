1419f531935760f98a210c44c9a7826c2c207882
Eira Mazon <eira.b.mazon@gmail.com>
2021-07-25 17:28:32 -0500

    initial set up commit

---
+++ cvgenerator/apps/cvgenerator/src/app/app.tsx
@@ -0,0 +1,14 @@
+import styles from './app.module.scss';
+
+import { ReactComponent as Logo } from './logo.svg';
+import star from './star.svg';
+
+import { Route, Link } from 'react-router-dom';
+
+export function App() {
+  return (
+   <h1>Hi</h1>
+  );
+}
+
+export default App;

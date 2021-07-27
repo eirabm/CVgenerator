6dc25babf10dceb3b13d8745799f92a770186325
Eira Mazon <eiramazon@Eiras-MacBook-Pro.local>
2021-07-27 12:23:37 -0500

    refactor. Mmoved sideBar layout to component lib

--- cvgenerator/apps/cvgenerator/src/app/app.tsx
+++ cvgenerator/apps/cvgenerator/src/app/app.tsx
@@ -1,34 +1,15 @@
 import styles from './app.module.scss';
-import {Account, Documents, Trash} from '@cvgenerator/containers';
 
-import {Header, SideBar} from '@cvgenerator/ui';
+import {Header} from '@cvgenerator/ui';
+import {SidebarLayout} from '@cvgenerator/layouts';
 
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 export function App() {
   return (
-  <div>
-
-   <Router>
-
+    <>
       <Header />
-      <SideBar />
-
-      <div className={styles.app}>
-
-        <Switch>
-          <Route exact path="/" component={Documents} />
-
-          <Route exact path="/trash" component={Trash} />
-
-          <Route exact path="/account" component={Account} />
-        </Switch>
-
-      </div>
-    </Router>
-
-
-  </div>
+      <SidebarLayout />
+    </>
   );
 }
 
--- cvgenerator/libs/layouts/src/lib/sidebar-layout/sidebar-layout.tsx
+++ cvgenerator/libs/layouts/src/lib/sidebar-layout/sidebar-layout.tsx
@@ -1,13 +1,35 @@
-import './sidebar-layout.module.scss';
+import styles from './sidebar-layout.module.scss';
+
+import {Account, Documents, Trash} from '@cvgenerator/containers';
+import {SideBar} from '@cvgenerator/ui';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 /* eslint-disable-next-line */
 export interface SidebarLayoutProps {}
 
 export function SidebarLayout(props: SidebarLayoutProps) {
   return (
-    <div>
-      <h1>Welcome to SidebarLayout!</h1>
-    </div>
+    <div className={styles.sideBar}>
+
+    <Router>
+ 
+        <SideBar />
+ 
+       <div>
+ 
+         <Switch>
+           <Route exact path="/" component={Documents} />
+ 
+           <Route exact path="/trash" component={Trash} />
+ 
+           <Route exact path="/account" component={Account} />
+         </Switch>
+ 
+       </div>
+     </Router>
+ 
+ 
+   </div>
   );
 }
 
